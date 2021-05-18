8aeba4ce61b8d26b5a100bb4e067328b24d2e6c8
Gagandeep Singh <809gagan@gmail.com>
2021-05-18 20:29:45 +0530

    git ignore added

---
+++ code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void IO()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+    IO();
+    int n;
+    cin >> n;
+    cout << n << endl;
+}
