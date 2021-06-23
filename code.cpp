70d0a497930af05f74431b7613db0609347e69e1
Gagandeep Singh <809gagan@gmail.com>
2021-06-23 23:36:29 +0530

    make it better

--- code.cpp
+++ code.cpp
@@ -1,123 +1,53 @@
-//------------Copyright Gagandeep Singh---------------------------
-
-
-//-----------------Import Headers and define namespaces------------
 #include <bits/stdc++.h>
 using namespace std;
-//-----------------------------------------------------------------
-
-
-//------------------Function for Input Output----------------------
-void IO()
-{
+#define ll long long
+long long MOD = 1e9 + 7;
+void IO() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
 }
-//----------------------------------------------------------------
 
-
-//-----------------Write Macros and Global here -------------------
-#define ll long long
-long long MOD = 1e9 + 7;
-//-----------------------------------------------------------------
-
-//---------------Write Functions and classes here------------------
-struct coder
-{
-    int first, second, index;
-} query[200000];
-/////////////////////////////////////////////////////////////////////////////////////////
-bool sorter(coder A, coder B)
-{
-
-    return A.second < B.second;
-}
-
-int bit[30001];
-////////////////////////////////////////////////////////////////////////////////////////
-void update(int index, int value, int n)
-{
-
-    for (; index <= n; index += index & (-index))
-    {
-        bit[index] += value;
-    }
-}
-int last[1000001];
-int arr[30001];
-///////////////////////////////////////////////////////////////////////////////////////////
-int value(int index)
-{
-
-    int res = 0;
-    for (; index > 0; index -= index & (-index))
-    {
-        res += bit[index];
-    }
-    return res;
-}
-
-//-----------------------------------------------------------------
-
-
-//--------------------Main functions-------------------------------
-int main()
-{
-    int n;
-    cin >> n;
-
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    int q;
-    cin >> q;
-
-    for (int i = 0; i < q; i++)
-    {
-        cin >> query[i].first >> query[i].second;
-        query[i].index = i;
-    }
-
-    sort(query, query + q, sorter);
-
-    memset(last, -1, sizeof(last));
-
-    int k = 0;
-    int *ans = new int[q];
-
-    int total = 0;
-    for (int i = 1; i <= n; i++)
-    {
-
-        if (last[arr[i]] != -1)
-        {
-            update(last[arr[i]], -1, n);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    IO();
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, w, r;
+        cin >> n >> w >> r;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
+
+        int start = 0;
+        int total = r;
+        sort(arr.begin(), arr.end());
+        while (start < n) {
+            int k = arr[start];
+
+            int count = 0;
+            while (start < n && arr[start] == k  ) {
+                start++;
+                count++;
+            }
+
+            if (count > 1) {
+                total += k * count;
+                if (count % 2 == 1) {
+                    total -= k;
+                }
+                if (total >= w)break;
+            }
+        }
+
+        if (total >= w)
+            cout << "YES" << endl;
         else
-        {
-            total++;
-        }
-
-        update(i, 1, n);
-        last[arr[i]] = i;
-
-        while (k < q && query[k].second == i)
-        {
-
-            ans[query[k].index] = total - value(query[k].first - 1);
-            k++;
-        }
+            cout << "NO" << endl;
     }
 
-    for (int i = 0; i < q; i++)
-    {
-        cout << ans[i] << endl;
-    }
-    return 0;
 }
-
-//------------------Do calculatios here----------------------------
